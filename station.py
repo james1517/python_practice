9ea8df9ab097f71385f00b8edebf964c55b77607
matsumura <g1672091@tcu.ac.jp>
2018-12-01 13:43:51 +0900

    hey you

--- station.py
+++ station.py
@@ -3,30 +3,22 @@ from selenium.webdriver.support.select import Select
 from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
+import csv
 
+
+print("出発駅を入力してください.")
+startStation = input()
+print("到着駅を入力してください.")
+endStation = input()
+print("月日時間を入力してください.(入力例：12/01/13/58)\n※現在時刻で取得する場合は「now」と入力してください。")
+dateTime = None
+while(dateTime == None):
+    dateTime = input()
 
 #ChromeDriverのパスを引数に指定しChromeを起動
 driver = webdriver.Chrome("c:/driver/chromedriver")
 #指定したURLに遷移
 driver.get("https://transit.yahoo.co.jp/")
-
-print("出発駅を入力してください.")
-startStation = input()
-print(startStation)
-
-print("到着駅を入力してください.")
-endStation = input()
-print(endStation)
-
-print("月日時間を入力してください.(入力例：12/01/13/58)")
-dateTime = input()
-dateTime = dateTime.split("/")
-dateTime = list(map(int, dateTime))
-print(dateTime)
-
-driver.find_element_by_id("sfrom").send_keys(startStation)
-driver.find_element_by_id("sto").send_keys(endStation)
-
 #idを取得
 month = driver.find_element_by_id("m")
 day = driver.find_element_by_id("d")
@@ -35,16 +27,23 @@ minute = driver.find_element_by_id("mm")
 #セレクトタグの要素を指定してSelectクラスのインスタンスを作成
 select = Select(month)
 
-#セレクトタグのオプションをインデックス番号から選択する
-#通し番号が０から始まっているため-1
-select.select_by_index(dateTime[0]-1)
+if(dateTime != "now"):
+    dateTime = dateTime.split("/")
+    dateTime = list(map(int, dateTime))
 
-select = Select(day)
-select.select_by_index(dateTime[1]-1)
-select = Select(hour)
-select.select_by_index(dateTime[2])
-select = Select(minute)
-select.select_by_index(dateTime[3])
+driver.find_element_by_id("sfrom").send_keys(startStation)
+driver.find_element_by_id("sto").send_keys(endStation)
+
+if(dateTime != "now"):
+    #セレクトタグのオプションをインデックス番号から選択する
+    #通し番号が０から始まっているため-1
+    select.select_by_index(dateTime[0]-1)
+    select = Select(day)
+    select.select_by_index(dateTime[1]-1)
+    select = Select(hour)
+    select.select_by_index(dateTime[2])
+    select = Select(minute)
+    select.select_by_index(dateTime[3])
 
 driver.find_element_by_id("searchModuleSubmit").click()
 
@@ -54,9 +53,12 @@ cur_url = driver.current_url
 # POOL_MNG = PoolManager()
 HTML = requests.get(cur_url)
 SOUP = BeautifulSoup(HTML.content, "html.parser")
-tmp = SOUP.select('#rsltlst .time span.mark')
-tmpA = tmp[0]
-print(tmpA)
+time_req = SOUP.select('#rsltlst .time span.small')
+price = SOUP.select('#rsltlst .fare span.mark')
+transfer = SOUP.select('#rsltlst .transfer')
+tmpA = time_req[0]
+priceA = price[0]
+transferA = transfer[0]
 print("所要時間：" + tmpA.getText())
-# print(SOUP.select('#rsltlst .time span.mark').getText())
-# print(SOUP.select('#rsltlst .fare span.mark').getText())
+print("料金：" + priceA.getText())
+print(transferA.getText())
